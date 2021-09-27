856ac70d1401cea8a323501dfe78464c0c3ff7ab
HeeYou Jang <gouk32@naver.com>
2021-09-27 21:45:04 +0900

    docs 수정하기

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,10 +1,13 @@
 import React, { HTMLAttributes, ReactNode } from 'react';
 
 export interface Props extends HTMLAttributes<HTMLButtonElement> {
+  /** Provide a text for the button */
   children: ReactNode;
+  /** Which variant look would you like use */
   variant?: 'Primary' | 'Secondary';
 }
 
+/** This is a special button!!! */
 export const Button = ({ children, variant = 'Primary', ...props }: Props) => {
   return (
     <button
